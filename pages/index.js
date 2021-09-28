61cf2ffb38be37662c72dfa8e86fc020faddcef4
GilsonGangadhar <gilsongangadhar@gmail.com>
2021-09-28 23:56:37 +0530

    creating main section ; trying to get data from server

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,19 @@ export default function Home() {
 
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          {/* Pull some data from a server - API */}
+        </section>
+      </main>
       
     </div>
   )
+}
+
+export async function getStaticProps() {
+  
 }
