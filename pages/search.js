4155a4101c21ac2cfa3cfb179f1f8d9710b217be
GilsonGangadhar <gilsongangadhar@gmail.com>
2021-09-30 01:44:14 +0530

    creating search page & styling

---
+++ pages/search.js
@@ -0,0 +1,31 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+function Search() {
+    return (
+        <div className="h-screen">
+            <Header/>
+
+            <main className="flex">
+                <section>
+                    <p className="text-xs">300+ stays for </p>
+
+                    <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in mars</h1>
+
+                    <div className="">
+                        <p className="button">Cancellation flexibility</p>
+                        <p className="button">Type of Place</p>
+                        <p className="button">Price</p>
+                        <p className="button">Rooms</p>
+                        <p className="button">More filters</p>
+                    </div>
+                </section>
+            </main>
+
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Search
